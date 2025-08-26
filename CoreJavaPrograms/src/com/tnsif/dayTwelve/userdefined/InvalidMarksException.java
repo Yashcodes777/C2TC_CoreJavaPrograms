778d0ea39a94b7b5c31e1a9d61aa482c0a93f8f2
umesh <umesh@hemanth.mshome.net>
2025-08-26 11:42:50 +0530

    Updated...

---
+++ CoreJavaPrograms/src/com/tnsif/dayTwelve/userdefined/InvalidMarksException.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayTwelve.userdefined;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
